36e12e69637db7343e8498643bda7d92d4717f1f
Pratik Patel <pratikpatel_85@yahoo.com>
2021-04-16 18:21:32 -0400

    Finished html, js, and css - PSP

---
+++ assets/js/script1.js
@@ -0,0 +1,27 @@
+// I put my question on another script sheet to organize my thoughts.
+var questions = [{
+        title: "What is Batman's real name?",
+        choices: ["Bruce Wayne", "John Wayne", "Clint Eastwood", "Michael Keaton"],
+        answer: "Bruce Wayne"
+    },
+    {
+        title: "Who is NOT a founding member of the Justice League?",
+        choices: ["Wonder Woman", "Batman", "Cyborg", "The Flash"],
+        answer: "Cyborg"
+    },
+    {
+        title: "Which Green Lantern was a member in Justice League Unlimited?",
+        choices: ["John Stewart", "Hal Jordan", "Kyle Rayner", "Both 1 and 2"],
+        answer: "Both 1 and 2"
+    },
+    {
+        title: "Who was the first Flash?",
+        choices: ["Barry Allen", "Jay Garrick", "Wally West", "Bart Allen"],
+        answer: "Jay Garrick"
+    },
+    {
+        title: "Which one of these Justice League Members do NOT have superpowers?",
+        choices: ["Superman", "Martian Manhunter", "Captain Atom", "Nightwing"],
+        answer: "Nightwing"
+    }
+]
